5f3dfc304da73fbd0907f80574ef023961902d40
neiforfaen <180103988+neiforfaen@users.noreply.github.com>
2026-06-20 18:09:16 +0200

    chore: scaffold default fumadocs app

---
+++ apps/docs/app/docs/layout.tsx
@@ -0,0 +1,35 @@
+import { buttonVariants } from "fumadocs-ui/components/ui/button"
+import { DocsLayout } from "fumadocs-ui/layouts/docs"
+import { MessageCircleIcon } from "lucide-react"
+import {
+  AISearch,
+  AISearchPanel,
+  AISearchTrigger,
+} from "@/components/ai/search"
+import { cn } from "@/lib/cn"
+import { baseOptions } from "@/lib/layout.shared"
+import { source } from "@/lib/source"
+
+export default function Layout({ children }: LayoutProps<"/docs">) {
+  return (
+    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+      <AISearch>
+        <AISearchPanel />
+        <AISearchTrigger
+          className={cn(
+            buttonVariants({
+              variant: "secondary",
+              className: "rounded-2xl text-fd-muted-foreground",
+            })
+          )}
+          position="float"
+        >
+          <MessageCircleIcon className="size-4.5" />
+          Ask AI
+        </AISearchTrigger>
+      </AISearch>
+
+      {children}
+    </DocsLayout>
+  )
+}
